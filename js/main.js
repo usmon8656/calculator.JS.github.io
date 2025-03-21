1c7119d98cff38f20d78a4f513a0e6f9eced4419
Norboyev Usmon <137477630+usmon8656@users.noreply.github.com>
2025-03-21 16:53:09 +0500

    site

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let display = document.querySelector(".display")
+
+let buttons = Array.from(document.querySelectorAll(".button"))
+
+
+buttons.map((button) =>{
+    button.addEventListener("click", (e)=>{
+
+        switch (e.target.innerText) {
+            case "AC":
+                try {
+                    display.innerText = "0";
+                } catch (e){
+                    display.innerText = "Error!";
+                }
+                break;
+            case "+/-":
+                display.innerText = "-";
+                break;
+            case "%":
+                let passedText = display.innerText + "/100";
+                display.innerText = eval(passedText);
+                break;
+            case "=":
+                display.innerText = eval(display.innerText);
+                break;
+            default: 
+            if (display.innerText==="0" && e.target.innerText !=="."){
+                display.innerText = e.target.innerText;
+            } else {
+                display.innerText += e.target.innerText;
+            }
+        }
+    })
+})
